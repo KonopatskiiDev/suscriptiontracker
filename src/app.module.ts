4544c5cb7b9dba9d45be866801d48996ec55f11f
unknown <kaba100@yandex.ru>
2026-04-24 17:43:08 +0700

    subscriptiontracker initialised project

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { SubscriptionsModule } from './subscriptions/subscriptions.module';
+import { PaymentsModule } from './payments/payments.module';
+
+@Module({
+  imports: [UsersModule, SubscriptionsModule, PaymentsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
